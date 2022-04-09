1ea3e46e53189a2fabd07dd252128e5b9c0d70e3
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-04-10 00:19:51 +0300

    New

---
+++ _.Net_Lesson_02_Operators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _.Net_Lesson_02_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
