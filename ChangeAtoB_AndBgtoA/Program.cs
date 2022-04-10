9906ec52ba15a6223c965993c69f302ad8cd3eae
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-04-10 20:12:57 +0300

    AtoBBtoA

---
+++ ChangeAtoB_AndBgtoA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChangeAtoB_AndBgtoA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A and B and press ENTER...");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c;
+
+            c = a;
+            a = b;
+            b = c;  
+            
+            Console.WriteLine($"a = {a}, b = {b}");
+            Console.ReadLine();
+        }
+    }
+}
