01e8bad0b47e53b2d6b82c0e08098ed3656ab42d
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-04-10 19:54:58 +0300

    Exrs.nr.4.Added

---
+++ ShowStringFourTimesINaRow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ShowStringFourTimesINaRow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any word and press ENTER...");
+            string text = Console.ReadLine();
+            PrintString(text);
+            Console.ReadLine();
+        }
+
+        
+        public static string PrintString(string text)
+        {
+            string result = (text + text + text + text);
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
